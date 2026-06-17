d60100d6d103028b2520696d84c6d24278148596
Evelyn Sales <evelynsales2402@gmail.com>
2026-06-17 20:38:13 -0300

    Feat: Inicio do projeto ionic e configuração capacitor

---
+++ android/app/src/main/java/com/evelyn/pokedexaf4/MainActivity.java
@@ -0,0 +1,5 @@
+package com.evelyn.pokedexaf4;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.evelyn.pokedexaf4',
+  appName: 'Pokédex AF4',
+  webDir: 'www'
+};
+
+export default config;
