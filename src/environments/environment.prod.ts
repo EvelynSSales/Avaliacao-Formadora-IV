6d4f2bcdb6e54520356b6aeb4bd00e1c58deeab5
Evelyn Sales <evelynsales2402@gmail.com>
2026-06-17 22:30:43 -0300

    fix: adiciona configuracao firebase no ambiente de producao

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyCh5Q5439eXieBufgH3goeybSxM7eEAkRw",
+    authDomain: "pokedex-af4.firebaseapp.com",
+    projectId: "pokedex-af4",
+    storageBucket: "pokedex-af4.firebasestorage.app",
+    messagingSenderId: "459539466527",
+    appId: "1:459539466527:web:39db75cbadfcbb4904c82a",
+    measurementId: "G-7HBP7NHV8H"
+  }
 };
