fc961183b4a7df338e790af5e28fa76c4b724b31
Evelyn Sales <evelynsales2402@gmail.com>
2026-06-17 22:12:53 -0300

    chore: silencia avisos de depreciação

--- src/app/pages/favoritos/favoritos.page.ts
+++ src/app/pages/favoritos/favoritos.page.ts
@@ -1,20 +1,65 @@
 import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule, TitleCasePipe } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { RouterLink } from '@angular/router';
+import {
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonButton,
+  IonButtons,
+  IonItem,
+  IonLabel,
+  IonInput
+} from '@ionic/angular/standalone';
+
+import { Favorito, FavoritosService } from '../../services/favoritos.service';
 
 @Component({
   selector: 'app-favoritos',
-  templateUrl: './favoritos.page.html',
-  styleUrls: ['./favoritos.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [
+    CommonModule,
+    FormsModule,
+    RouterLink,
+    TitleCasePipe,
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonButton,
+    IonButtons,
+    IonItem,
+    IonLabel,
+    IonInput
+  ],
+  templateUrl: './favoritos.page.html',
+  styleUrls: ['./favoritos.page.scss']
 })
 export class FavoritosPage implements OnInit {
+  favoritos: Favorito[] = [];
 
-  constructor() { }
+  constructor(private favoritosService: FavoritosService) {}
 
   ngOnInit() {
+    this.favoritosService.listar().subscribe(lista => {
+      this.favoritos = lista;
+    });
   }
 
+  atualizar(favorito: Favorito) {
+    if (!favorito.id) return;
+
+    this.favoritosService.atualizar(
+      favorito.id,
+      favorito.observacao
+    );
+  }
+
+  remover(favorito: Favorito) {
+    if (!favorito.id) return;
+
+    this.favoritosService.remover(favorito.id);
+  }
 }
